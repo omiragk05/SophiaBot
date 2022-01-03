a5e2e2497839c9a67909327ee59c9793cb985873
omira geevin <92523621+omiragk05@users.noreply.github.com>
2022-01-03 16:49:46 +0530

    Update country.py

--- SophiaBot/modules/country.py
+++ SophiaBot/modules/country.py
@@ -101,7 +101,7 @@ Time Zones:- {tom}
 Top Level Domain:- {lanester}
 wikipedia:- {wiki}</b>
 
-Gathered By @SophiaSLBot.</b>
+Gathered By @MissAnna_robot.</b>
 """
     
     
@@ -120,7 +120,7 @@ __help__ = """
 *Commands*
 - /country [country name]*:* Gathering info about given country
 
-@SophiaUpdates
+@MissAnna_robot
 """
 
 __mod_name__ = "Country"
